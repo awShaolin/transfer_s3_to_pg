3e259d46b75db407a6f8561a734ca14493b5ba2d
Матвей Мешков <meshkov@aeroidea.ru>
2024-11-29 01:17:25 +0300

    testing

--- dags/libs/s3_to_postgres.py
+++ dags/libs/s3_to_postgres.py
@@ -2,6 +2,8 @@ import os
 import re
 import logging
 import pandas as pd
+import polars as pl
+from io import StringIO
 from sqlalchemy import text
 from sqlalchemy.exc import SQLAlchemyError
 from datetime import datetime
@@ -14,27 +16,22 @@ def process_table_files(bucket, table_name, date_start, date_end):
 
     s3 = S3Handler()
 
-    # check pg connetion 
     pg_engine = get_connection_postgres()
     try:
         with pg_engine.connect() as connection:
             connection.execute("SELECT 1")
-        print(">>> Connection to postgresql successful!")
+        logging.info(">>> Connection to PostgreSQL successful!")
     except SQLAlchemyError as e:
-        error_message = str(e.__dict__['orig'])
-        print(f">>> Error connecting to PostgreSQL: {error_message}")
+        logging.error(f">>> Error connecting to PostgreSQL: {str(e)}")
         raise
-    except Exception as e:
-        print(f">>> An unexpected error occurred: {str(e)}")
-        raise
-    
+
     s3_file_prefix = f"dbo.{table_name}_"
     formated_table_name = camel_to_snake(table_name)
     schema_name = bucket
 
     logging.info(f">>> Start transfer from S3 files - {table_name} to PostgreSQL table - {formated_table_name}.")
-    
-    # get list of files from s3
+
+    # Получение списка файлов из S3
     all_files = s3.list_objects_v2(Bucket=bucket, Prefix=s3_file_prefix)["Contents"]
     date_start_s3 = datetime.strptime(date_start, "%Y-%m-%d").strftime("%Y%m%d")
     date_end_s3 = datetime.strptime(date_end, "%Y-%m-%d").strftime("%Y%m%d")
@@ -44,24 +41,20 @@ def process_table_files(bucket, table_name, date_start, date_end):
     ]
 
     logging.info(f">>> Found {len(filtered_files)} files in S3 for the period from {date_start} to {date_end}.")
-
-    # load files to df
+    
     dataframes = []
     row_cnt = 0
     for s3_key in filtered_files:
         obj = s3.get_object(Bucket=bucket, Key=s3_key)
-        df = pd.read_csv(obj["Body"], delimiter=';')
-
         file_timestamp = s3_key.split("_")[-1].split(".")[0]
-        df["s3_file_timestamp"] = file_timestamp
 
-        df.columns = [camel_to_snake(col) for col in df.columns]
-
-        df = df.where(pd.notnull(df), None)
-        df = df.applymap(lambda x: None if pd.isna(x) else x)
+        df = pl.read_csv(obj["Body"], separator=";", infer_schema=False)
+        df = df.with_columns(pl.lit(file_timestamp).alias("s3_file_timestamp"))
+        df = df.with_columns([pl.col(col).cast(pl.Utf8) for col in df.columns])
+        df = df.rename({col: camel_to_snake(col) for col in df.columns})
 
         dataframes.append(df)
-        row_cnt += len(df)
+        row_cnt += df.shape[0]
 
     logging.info(f">>> Formed {len(dataframes)} dataframes from S3 files.")
     logging.info(f">>> Total rows before concatenation: {row_cnt}.")
@@ -71,89 +64,63 @@ def process_table_files(bucket, table_name, date_start, date_end):
         return 
     
     combined_df = (
-        pd.concat(dataframes)
-        .sort_values(by=["s3_file_timestamp"], ascending=True)  
-        .drop_duplicates(subset=["id"], keep="last") 
+        pl.concat(dataframes)
+        .sort(by=["s3_file_timestamp"], descending=False)  
+        .unique(subset=["id"], keep="last")
     )
 
     logging.info(f">>> Total rows after concatenation and deduplication: {len(combined_df)}.")
 
-    # check postgres table 
     with pg_engine.connect() as conn:
-        logging.info(">>> Checking if table exists in PostgreSQL...")
         table_exists = conn.execute(
             text(f"SELECT to_regclass('{schema_name}.{formated_table_name}') IS NOT NULL;")
         ).scalar()
-        logging.info(f">>> Table exists: {table_exists}")
-
         if not table_exists:
-            ddl = generate_ddl(schema_name, formated_table_name, combined_df)
+            ddl = f"""
+                CREATE TABLE {schema_name}.{formated_table_name} (
+                    {', '.join([f"{col} TEXT" for col in combined_df.columns])}
+                );
+            """
+            logging.info(f">>> Creating table {schema_name}.{formated_table_name} with DDL: {ddl}")
             conn.execute(text(ddl))
 
     with pg_engine.begin() as transaction:
         try:
-            perform_upsert(pg_engine, schema_name, formated_table_name, combined_df)
+            load_to_pg(pg_engine, schema_name, formated_table_name, combined_df)
         except Exception as e:
-            logging.error(f"Error during upsert into {formated_table_name}: {e}")
-            transaction.rollback()  
+            logging.error(f"Error during COPY to {schema_name}.{formated_table_name}: {e}")
             raise
 
-def generate_ddl(schema_name, table_name, df):
-    columns = []
-
-    dtype_mapping = {
-        'int64': 'BIGINT',
-        'float64': 'DOUBLE PRECISION',
-        'object': 'TEXT',
-        'bool': 'BOOLEAN',
-        'datetime64[ns]': 'TIMESTAMP',
-    }
-
-    for col, dtype in df.dtypes.items():
-        pg_type = dtype_mapping.get(str(dtype), 'TEXT')
-        columns.append(f"{col} {pg_type}")
-
-    if "id" not in df.columns:
-        raise ValueError("Column 'id' is required for primary key but is missing in DataFrame.")
-
-    columns.append("PRIMARY KEY (id)")
-
-    ddl = f"CREATE TABLE {schema_name}.{table_name} ({', '.join(columns)});"
-    logging.info("Create table with ddl: ")
-    logging.info(ddl)
-    return ddl
-
-
-def perform_upsert(engine, schema_name, table_name, df):
-    insert_cnt = 0
-    update_cnt = 0
+def load_to_pg(engine, schema_name, table_name, df):
+    batch_size = 100000
 
     with engine.connect() as conn:
-        for _, row in df.iterrows():
-            row_dict = row.to_dict()
-            columns = ', '.join(row_dict.keys())
-            placeholders = ', '.join([f":{key}" for key in row_dict.keys()])
-            updates = ', '.join([f"{key} = EXCLUDED.{key}" for key in row_dict.keys()])
+        ids_to_delete = tuple(df["id"].unique().to_list())
+        if ids_to_delete:
+            delete_query = text(f"DELETE FROM {schema_name}.{table_name} WHERE id IN :ids;")
+            conn.execute(delete_query, {"ids": ids_to_delete})
 
-            upsert_query = text(f"""
-                INSERT INTO {schema_name}.{table_name} ({columns})
-                VALUES ({placeholders})
-                ON CONFLICT (id) DO UPDATE
-                SET {updates}
-                RETURNING (xmax = 0) AS is_inserted;
-            """)
+        for i in range(0, len(df), batch_size):
+            batch_df = df.slice(i, batch_size)
+            copy_data_to_postgres(conn, schema_name, table_name, batch_df)
 
-            try:
-                result = conn.execute(upsert_query, row_dict).fetchone()
-                if result and result["is_inserted"]:
-                    insert_cnt += 1
-                else:
-                    update_cnt += 1
-            except Exception as e:
-                logging.error(f">>> Error upserting row with id={row_dict['id']} into {schema_name}.{table_name}: {e}")
-                raise
+def copy_data_to_postgres(conn, schema_name, table_name, df):
+    buffer = StringIO()
+    df.write_csv(buffer, separator="\t", null_value="\\N")
+    buffer.seek(0)
 
-    logging.info(f">>> Upsert completed for table {table_name}: {insert_cnt} rows inserted, {update_cnt} rows updated, total rows processed - {insert_cnt + update_cnt}.")
+    copy_query = f"""
+        COPY {schema_name}.{table_name} ({', '.join(df.columns)}) 
+        FROM STDIN WITH (FORMAT CSV, DELIMITER '\t', NULL '\\N');
+    """
+    try:
+        conn.connection.cursor().copy_expert(copy_query, buffer)
+        logging.info(f">>> Batch of {len(df)} rows copied to {schema_name}.{table_name}.")
+    except Exception as e:
+        logging.error(f"Error during COPY to {schema_name}.{table_name}: {e}")
+        raise
+
+
 
 def camel_to_snake(name):
     s1 = re.sub('([a-z0-9])([A-Z])', r'\1_\2', name)
